b42b638c686325feff900d00fc34e5873ed87654
misaka-10031 <68635194+misaka-10031@users.noreply.github.com>
2022-06-14 14:20:49 +0800

    更新了报错说明

--- plugin/weather.py
+++ plugin/weather.py
@@ -8,9 +8,9 @@ def get(city):
     r = requests.get(url=url.format(city))
     try:
         if r.json()["errcode"]==100:
-            return "未知城市"
+            return "未知城市！仅支持中国(包含港澳台)市级与县级天气查询！"
     except:
-        print("未知城市")
+        print("未知城市！仅支持中国(包含港澳台)市级与县级天气查询！")
     data = r.json()["data"]
     return ("{}:\n".format(city) +
             "天气:{}".format(data[0]["wea"]) +
